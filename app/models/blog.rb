0fd50e8cb682015cbba0ca989fa28f6634c53007
jouishi <08251023jke01@gmail.com>
2023-08-02 17:42:47 +0900

    コメント付き投稿削除修正

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,7 +2,8 @@ class Blog < ApplicationRecord
   mount_uploader :movie, MovieUploader
   mount_uploader :image, ImageUploader
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
+
   def self.search(search)
     if search != ""
       Blog.where('text LIKE(?)', "%#{search}%")
